ee5a9b5562c58d91fa1d2a11244bbaa542cc1256
Manel Arrufat <manelarrufat@gmail.com>
2018-04-07 13:42:40 +0200

    Creada la clase MessageListBuilder

--- src/MessageListBuilder.php
+++ src/MessageListBuilder.php
@@ -5,6 +5,12 @@ namespace Drupal\forcontu_entities;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
+use Drupal\Core\Routing\LinkGeneratorTrait;
+use Drupal\Core\Url;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 
 /**
  * Defines a class to build a listing of Message entities.
@@ -13,13 +19,40 @@ use Drupal\Core\Link;
  */
 class MessageListBuilder extends EntityListBuilder {
 
+  use LinkGeneratorTrait;
+  
+  protected $dateFormatter;
+  
+  public function __construct(EntityTypeInterface $entity_type, 
+EntityStorageInterface $storage, 
+DateFormatterInterface $date_formatter) {
+    parent::__construct($entity_type, $storage);
+    
+    $this->dateFormatter = $date_formatter;
+  }
 
   /**
    * {@inheritdoc}
    */
+  public static function createInstance(ContainerInterface $container, 
+EntityTypeInterface $entity_type) {
+    return new static (
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('date.formatter')
+    );
+  }
+
+    /**
+   * {@inheritdoc}
+   */
   public function buildHeader() {
     $header['id'] = $this->t('Message ID');
-    $header['name'] = $this->t('Name');
+    $header['from'] = $this->t('From');
+    $header['to'] = $this->t('To');
+    $header['subject'] = $this->t('Subject');
+    $header['created'] = $this->t('Created');
+    
     return $header + parent::buildHeader();
   }
 
@@ -29,11 +62,17 @@ class MessageListBuilder extends EntityListBuilder {
   public function buildRow(EntityInterface $entity) {
     /* @var $entity \Drupal\forcontu_entities\Entity\Message */
     $row['id'] = $entity->id();
-    $row['name'] = Link::createFromRoute(
+    $row['from'] = $entity->getOwner()->getAccountName();
+    $row['to'] = $entity->getUserTo()->getAccountName();
+    $row['subject'] = $this->l(
       $entity->label(),
-      'entity.forcontu_entities_message.edit_form',
-      ['forcontu_entities_message' => $entity->id()]
+      new Url(
+        'entity.forcontu_entities_message.edit_form', array(
+          'forcontu_entities_message' => $entity->id(),
+        )
+      )
     );
+        
     return $row + parent::buildRow($entity);
   }
 
